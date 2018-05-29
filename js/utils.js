67e7901232524937c4ed8784de66b9da48152ce5
YuanwaiLi <1426890675@qq.com>
2018-05-29 23:17:20 +0800

    lint

--- js/utils.js
+++ js/utils.js
@@ -220,20 +220,16 @@
       return Math.floor(Math.round((time - checkDate) / 86400000) / 7) + 1
     },
     addHandler: function(element, type, handler) {
-    	if (element.addEventListener) 
-    		element.addEventListener(type, handler, false)
-    	else if (element.attachEvent)
-    		element.attachEvent('on' + type, handler)
-    	else 
-    		element['on' + type] = handler
+      if (element.addEventListener)
+        element.addEventListener(type, handler, false)
+      else if (element.attachEvent) element.attachEvent('on' + type, handler)
+      else element['on' + type] = handler
     },
     removeHandler: function(element, type, handler) {
-    	if (element.removeEventListener) 
-    		element.removeEventListener(type, handler, false)
-    	else if (element.detachEvent)
-    		element.detachEvent('on' + type, handler)
-    	else 
-    		element['on' + type] = null
+      if (element.removeEventListener)
+        element.removeEventListener(type, handler, false)
+      else if (element.detachEvent) element.detachEvent('on' + type, handler)
+      else element['on' + type] = null
     }
   }
 
