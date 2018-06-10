68e971c21011bdabb0928dba594c073839d22473
YuanwaiLi <1426890675@qq.com>
2018-06-11 00:26:07 +0800

    add loader

--- js/moon.js
+++ js/moon.js
@@ -165,8 +165,398 @@ namespace('moon.base')
 // !!!
 moon.base.extend(window, moon.base)
 
-/**================================================浏览器类型和版本号=======================================================*/
-/**================================================字符串工具类============================================================*/
+/**================================================loader=======================================================*/
+namespace('moon.loader')
+;(function() {
+  var loader = moon.loader,
+    __loading = null,
+    loaded = {}, //是否下载
+    loading = {}, //正在下载  下载前就建立，以处理同时多次请求，但此处写了，就对不住loaded了，需重构
+    queue = [] //define的deps队列
+  extend(loader, {
+    modulemap: {},
+    defineMap: [],
+    isMainLoaded: 0,
+    preNeed: function() {
+      loader.need(loader.defineMap, null)
+    },
+    /**
+     * 加载对象方法 对应模块需有define方法定义，否则返回中无法使用
+     * @param {array} modules 模块名称
+     * @param {function} cb 回调, 回调方法中的参数为加载模块的返回
+     * @return {undefined} undefined
+     */
+    need: function(modules, cb) {
+      if (!isArray(modules)) {
+        modules = new Array(modules)
+      }
+      var mc = moduleContainer('', modules, cb)
+      start(mc)
+    },
+    /**
+     * 模块定义方法
+     * @param {string} name 下载对象
+     * @param {array} modules 下载对象
+     * @param {function} callback 下载对象
+     * @return {undefined} undefined
+     */
+    define: function(name, deps, cb) {
+      //无name参数时从文件名取name（urlcb回调中处理）
+      //无deps，则[]
+      if (!isString(name)) {
+        callback = deps
+        deps = name
+        name = 'noname_' + Math.floor(Math.random() * 1000000)
+      }
+      if (!isArray(deps)) {
+        callback = deps
+        deps = []
+        name = 'noname_' + Math.floor(Math.random() * 1000000)
+      }
+      //**callback非function 为object时，直接为name返回object（cb回调中）
+      queue.push([name, deps, callback])
+      loader.defineMap.push(name)
+    },
+    /**
+     * 加载文件方法(多个文件)
+     * 适合进行跨域请求
+     * @public
+     * @param {array} filepaths 需要加载脚本
+     * @param {function} callback 回调方法中带有一参数表明加载是否成功。
+     * @return {undefined} 无
+     */
+    include: function(filepaths, cb) {
+      var files = new Array()
+      files = files.concat(filepaths)
+      if (!isFunction(cb)) cb = function() {}
+      var ic = includerContainer(files, cb)
+      start(ic)
+    },
+    loadCSS: function(url, cb) {
+      if (url.search(/^https?:\/\//i) == -1) {
+        url =
+          moon.config.loaderPath +
+          url.replace(/\./g, '/') +
+          '.css' +
+          '?' +
+          moon.config.version
+      }
+      var isCSS = /\.css(\?|$)/i.test(url)
+      if (!isFunction(cb)) cb = function() {}
+      if (isCSS & !loaded[url]) {
+        loadCSS(url, cb)
+        loaded[url] = true
+      }
+    },
+    loadScript: function(url, cb) {
+      if (!isFunction(cb)) cb = function() {}
+      loadScript(url, cb)
+    }
+  })
+  loader.define.amd = {
+    jQuery: true
+  }
+  /**
+   * start加载开始
+   * @private
+   * @param {object} mc 下载对象
+   * @return {undefined} undefined
+   */
+  function start(mc) {
+    var need = mc.need
+    for (var key in need) {
+      // console.log(key+" -> "+moon.loader.modulemap[key])
+      //如果modulemap里有，就直接返回
+      if (moon.loader.modulemap[key]) {
+        mc.loadUrlCallback.apply(mc, [moon.loader.modulemap[key], key])
+      } else {
+        //判断有没有加载过
+        var loaded = false
+        for (var i = 0; i < queue.length; i++) {
+          if (queue[i][0] == key) {
+            mc.loadUrlCallback.apply(mc, [queue[i][2], key, i])
+            loaded = true
+            break
+          }
+        }
+        if (!loaded) load(need[key], key, mc)
+      }
+    }
+    //检查加载状态
+    checkloaded(mc)
+  }
+  /**
+   * 下载文件
+   * @private
+   * @param {object} mc 下载对象
+   * @return {undefined} undefined
+   */
+  function load(url, name, mc) {
+    var isCSS = /\.css(\?|$)/i.test(url)
+    loading[name] = true
+    isCSS
+      ? loadCSS(url, function(ret) {
+          mc.loadUrlCallback.call(mc, [ret, name])
+        })
+      : loadScript(url, function(ret) {
+          mc.loadUrlCallback.apply(mc, [ret, name])
+        })
+  }
+
+  /**
+   * 检查加载情况
+   * @private
+   * @param {object} mc 下载对象
+   * @return {undefined} undefined
+   */
+  function checkloaded(mc) {
+    //加载完成
+    if (mc.hasdown == mc.needown) {
+      mc.loadInluderCallback.apply(mc, [true])
+      return
+    }
+    //加载超时
+    if (mc.hasdown < mc.needown && mc.expires <= 0) {
+      //**不用等expires,对单个文件失败的可以提前判断
+      mc.loadInluderCallback.apply(mc, [false])
+      return
+    }
+    //继续监听
+    if (mc.hasdown < mc.needown && mc.expires > 0) {
+      mc.expires = mc.expires - 50
+      setTimeout(function() {
+        checkloaded(mc)
+      }, 50)
+    }
+  }
+
+  /************************************includer处理******************************************/
+  /**
+   * includer内容加载器
+   * @private
+   * @param {string} name 下载名称
+   * @param {array} files 依赖模块对象
+   * @param {function} callback 回调方法
+   * @return {object} object
+   */
+  function includerContainer(files, callback) {
+    var needown = 0,
+      hasdown = 0,
+      need = {}
+
+    for (var i = 0; i < files.length; i++) {
+      var url = getModulePath(files[i])
+      needown++
+      if (loaded[files[i]]) {
+        hasdown++
+        break
+      }
+      need[files[i]] = url
+    }
+    return {
+      files: files,
+      need: need, //依赖对象数组(用于load下载)
+      res: new Array(), //依赖对象结果 结果集
+      expires: needown * milo.config.expires, //过期时间
+      callback: callback, //模块加载完成后的回调
+      needown: needown, //需要下载数
+      hasdown: hasdown, //已下载数
+      /**
+       * 单文件下载成功后回调
+       * @private
+       * @param {bool} ret 下载情况
+       * @param {string} name 模块名称
+       * @return {undefined} undefined
+       * 获取文件内的define对象，创建新mc
+       */
+      loadUrlCallback: function(ret, name) {
+        if (ret) this.hasdown++
+        loaded[name] = ret
+      },
+      /**
+       * 所有文件下载成功后回调
+       * @private
+       */
+      loadInluderCallback: function(ret) {
+        var res = []
+        for (var i = 0; i < this.files.length; i++) {
+          res.push(loaded[this.files[i]])
+        }
+        this.callback.apply(null, res)
+      }
+    }
+  }
+  /************************************加载脚本通用方法**************************************/
+  /**
+   * 加载样式基础方法
+   * 暂不处理加载的情况，均认为加载成功。
+   * @private
+   */
+  function loadCSS(url, callback) {
+    var head = document.getElementsByTagName('head')[0]
+    var link = head.appendChild(document.createElement('link'))
+    link.href = url
+    link.rel = 'stylesheet'
+    callback.call(this, true)
+  }
+
+  /**
+   * 加载脚本基础方法
+   * @private
+   * @param {string} filepath 路径
+   * @param {function} callback 回调方法
+   * @return {undefined} undefined
+   */
+  function loadScript(url, callback) {
+    var head = document.getElementsByTagName('head')[0]
+    var script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = url
+    var timeout = setTimeout(function() {
+      head.removeChild(script)
+      callback.call(this, false)
+    }, moon.config.expires)
+    onload(script, function(Ins) {
+      head.removeChild(script)
+      clearTimeout(timeout)
+      callback(true)
+    })
+    head.appendChild(script)
+    return true
+  }
+  /**
+   * 加载脚本完成后的处理
+   * @private
+   * @param {dom} node script DOM
+   * @param {function} callback 回调方法
+   * @return {undefined} undefined
+   */
+  function onload(node, callback) {
+    var isImpOnLoad =
+      'onload' in node
+        ? true
+        : (function() {
+            node.setAttribute('onload', '')
+            return typeof node.onload == 'function'
+          })()
+
+    if (document.addEventListener) {
+      node.addEventListener(
+        'load',
+        function() {
+          callback.call(this, node)
+        },
+        false
+      )
+    } else if (!isImpOnLoad) {
+      node.attachEvent('onreadystatechange', function() {
+        var rs = node.readyState.toLowerCase()
+        if (rs === 'loaded' || rs === 'complete') {
+          node.detachEvent('onreadystatechange')
+          callback.call(this, node.innerHTML)
+        }
+      })
+    } else {
+      //maybe someother browser
+    }
+  }
+  /**
+   * 获取脚本路径
+   * 以http://开头的，为fullpath
+   * 其它的均视为相对路径
+   * 组合路径或模块方式考虑中
+   * @private
+   * @param {string} filepath 路径
+   * @return {string} fullpath 全局路径
+   */
+  function getModulePath(filepath) {
+    if (filepath.search(/^https?:\/\//i) == -1) {
+      filepath =
+        moon.config.loaderPath +
+        filepath.replace(/\./g, '/') +
+        '.js' +
+        '?' +
+        moon.config.version
+    }
+    return filepath
+  }
+
+  /**
+   * 内容加载器
+   * @private
+   * @param {string} name 下载名称
+   * @param {array} modules 依赖模块对象
+   * @param {function} callback 回调方法
+   * @return {object} object
+   * 每一个命令对需要下载的文件即增加一个加载器
+   */
+  function moduleContainer(name, modules, cb) {
+    var needown = 0,
+      hasdown = 0,
+      hasmaped = 0,
+      need = {}
+    for (var i = 0; i < modules.length; i++) {
+      var url = getModulePath(modules[i])
+      needown++
+      //已下载过的模块进行处理，表明已下载
+      //但不处理maped情况，由回调统一处理
+      if (modules[i] == 'util.zepto' && typeof Zepto !== 'undefined') {
+        moon.loader.modulemap['util.zepto'] = Zepto
+        hasdown++
+        continue
+      } else {
+        if (loaded[modules[i]] || loading[modules[i]]) {
+          hasdown++
+          continue
+        }
+        need[modules[i]] = url
+      }
+    }
+    return {
+      name: name, //模块名
+      modules: modules, //依赖模块名
+      need: need, //依赖对象数组(用于load下载)
+      res: new Array(), //依赖对象结果 结果集
+      //**对于对象的时间处理还需要调整
+      expires: modules.length * moon.config.expires, //过期时间
+      callback: callback, //模块加载完成后的回调
+      needown: needown, //需要下载数
+      hasdown: hasdown, //已下载数
+      hasmaped: hasmaped, //已成功定义数
+      /**
+       * 单文件下载成功后回调
+       * @param {bool} ret 下载情况
+       * @param {string} name 模块名称
+       * @param {string} startPos 开始取的位置,提供对具名函数打乱次序的加载支持
+       * @return {undefined} undefined
+       * 获取文件内的define对象，创建新mc
+       */
+      loadUrlCallback: function(ret, name, startPos) {
+        //无论是否成功都增加已下载数，表明已处理
+        this.hasdown++
+        if (ret) {
+          loaded[name] = true
+          if (!startPos) startPos = 0
+          var deps = queue.splice(startPos, 1).pop()
+          if (deps === null) {
+            moon.loader.modulemap[name] = ret
+            remoduleContainerturn
+          }
+          deps[0] = name
+          //每新建一个deps则处理
+          var mc = moduleContainer.apply(null, deps)
+          start(mc)
+        } else {
+          //失败提前处理结果
+          moon.loader.modulemap[name] = 'undefined'
+        }
+      }
+    }
+  }
+})()
+extend(window, moon.loader)
+/**================================================浏览器类型和版本号===================================================*/
+/**================================================字符串工具类=========================================================*/
 namespace('moon.string')
 ;(function() {
   var string = moon.string
@@ -487,7 +877,7 @@ namespace('moon.number')
   })
 })()
 
-/**======================================================array工具类=============================================================*/
+/**=================================================array工具类================================================*/
 namespace('moon.array')
 ;(function() {
   var array = moon.array
@@ -571,7 +961,7 @@ namespace('moon.array')
   })
 })()
 
-/**======================================================cookie工具类=============================================================*/
+/**===============================================cookie工具类==============================================*/
 namespace('moon.cookie')
 ;(function() {
   var cookie = moon.cookie
@@ -827,7 +1217,6 @@ namespace('moon.event')
   //   if (!moon.isReady) {
   //     if (!document.body) return setTimeout(ready, 15)
   //     moon.isReady = true
-
   //   }
   // }
 })()
